1983dc409dab94389d1af652a3a1d794dacf31d1
BenRaymon <bendraymon@gmail.com>
2021-10-18 13:34:44 -0400

    email validation

--- src/app/rush/rush.component.ts
+++ src/app/rush/rush.component.ts
@@ -12,16 +12,30 @@ constructor() { }
 
 ngOnInit(): void {
     document.querySelectorAll('.input2').forEach((item)=>{
-    item.addEventListener('blur', ()=>{
 
-        if((<HTMLInputElement>item).value != "") {
-            (item).classList.add('has-val');
-        }
-        else {
-            (item).classList.remove('has-val');
-        }
-    })    
-})
+        //on blur add or remove has-val class
+        item.addEventListener('blur', ()=>{
+
+            if((<HTMLInputElement>item).value != "") {
+                (item).classList.add('has-val');
+
+                //Check validation for email
+                if(item.getAttribute('name') == 'email'){
+                    let regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+                    if((<HTMLInputElement>item).value.match(regexp)){
+                        item.parentElement?.children[1].classList.remove('no-validate');
+                        item.parentElement?.children[1].setAttribute('data-placeholder', 'EMAIL');
+                    } else {
+                        item.parentElement?.children[1].classList.add('no-validate');
+                        item.parentElement?.children[1].setAttribute('data-placeholder', 'EMAIL: Please enter valid email');
+                    }
+                }
+            }
+            else {
+                (item).classList.remove('has-val');
+            }
+        });    
+    });
 
 }
 
