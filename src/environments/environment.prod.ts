002a3857bd173efac984e43c8fa98769f25549de
BenRaymon <bendraymon@gmail.com>
2021-10-18 21:40:53 -0400

    ghpages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD0Gx0f94G6tpCMEG7Q5reUoqGRKj01a5I",
+    authDomain: "aepi-5f3da.firebaseapp.com",
+    databaseURL: "https://aepi-5f3da-default-rtdb.firebaseio.com",
+    projectId: "aepi-5f3da",
+    storageBucket: "aepi-5f3da.appspot.com",
+    messagingSenderId: "836069418728",
+    appId: "1:836069418728:web:e3b0431b44349038e70b5d",
+    measurementId: "G-M2SVY46KLG"
+  }
 };
