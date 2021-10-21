85ec63df1ab8a10be698bab6ccb4d92f3487a83e
BenRaymon <bendraymon@gmail.com>
2021-10-21 17:03:37 -0400

    change colors, new style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 //import { environment } from "src/environments/environment";
-//import { AngularFireModule } from '@angular/fire/compat';
-//import { AngularFireDatabase } from '@angular/fire/compat/database';
+//import { AngularFireModule } from "@angular/fire/compat";
+//import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+//import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { FormsModule } from '@angular/forms';
-//import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
