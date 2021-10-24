d95e4495803e1b9975329a25df6f59d29afcd8d4
BenRaymon <bendraymon@gmail.com>
2021-10-24 16:39:26 -0400

    route home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     component:HomeComponent
   },
   {
+    path:'',
+    component:HomeComponent
+  },
+  {
     path:'brotherhood',
     component:BrotherhoodComponent
   },
